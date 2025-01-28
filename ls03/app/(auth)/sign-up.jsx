e33ec774907af4c5561f67e09577a86304ce480b
Eamon Ali <eamonmondol525@gmail.com>
2025-01-28 19:39:32 +0600

    completed sign up page

--- ls03/app/(auth)/sign-up.jsx
+++ ls03/app/(auth)/sign-up.jsx
@@ -1,16 +1,30 @@
-import { View, Text, ScrollView, Image } from 'react-native'
+import { View, Text, ScrollView, Image, Alert } from 'react-native'
 import React, { useState } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { images } from '../../constants'
 import FormField from '../../components/FormField'
 import CustomButton from '../../components/CustomButton'
-import { Link } from 'expo-router'
+import { Link, router } from 'expo-router'
+import { createUser } from '../../lib/appwrite'
 
 const SignUp = () => {
   const [form, setForm] = useState({username: '', email: '', password: ''})
   const [isSubmitting, setIsSubmitting] = useState(false)
 
-  const submit = ()=> {}
+  const submit = async ()=> {
+    if(!form.username || !form.email || !form.password) {
+      Alert.alert("Error", "Please fill in all the fields")
+    }
+    setIsSubmitting(true)
+    try {
+      const result = await createUser(form.email, form.password, form.username);
+      router.replace('/home')
+    } catch (error) {
+      Alert.alert("Error", error.message)
+    } finally {
+      setIsSubmitting(false)
+    }
+  }
   return (
     <SafeAreaView className='bg-primary h-full'>
       <ScrollView>
@@ -22,7 +36,7 @@ const SignUp = () => {
           <FormField title='Email' value={form.email} handleChangeText={(e) => setForm({...form, email: e})} otherStyles="mt-7" keyboardType='email-address'/>
           <FormField title='Password' value={form.password} handleChangeText={(e) => setForm({...form, password: e})} otherStyles="mt-7"/>
           
-          <CustomButton title='Sign in' handlePress={submit} containerStyles='mt-14' isLoading={isSubmitting}/>
+          <CustomButton title='Sign up' handlePress={submit} containerStyles='mt-14' isLoading={isSubmitting}/>
           <View className='justify-center pt-5 flex-row gap-2'>
             <Text className='text-lg text-gray-100'>Have an account already?</Text>
             <Link href='/sign-in' className='text-lg text-secondary font-bold'>Sign In</Link>
