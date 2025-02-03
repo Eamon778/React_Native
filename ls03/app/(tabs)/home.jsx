405873180230cb8440ac24656f773a647e8540fe
Eamon Ali <eamonmondol525@gmail.com>
2025-02-03 12:00:55 +0600

    adding some home features

--- ls03/app/(tabs)/home.jsx
+++ ls03/app/(tabs)/home.jsx
@@ -1,14 +1,23 @@
-import { View, Text, FlatList, Image } from 'react-native'
+import { View, Text, FlatList, Image, RefreshControl } from 'react-native'
 import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { images } from '../../constants'
 import SearchInput from '../../components/SearchInput'
+import Trending from '../../components/Trending'
+import EmptyState from '../../components/EmptyState'
+import { useState } from 'react'
 
 const Home = () => {
+  const [refreshing, setRefreshing] = useState(false)
+  const onRefresh = async ()=>{
+    setRefreshing(true)
+    //re call videos
+    setRefreshing(false)
+  }
   return (
-    <SafeAreaView className='bg-primary'>
+    <SafeAreaView className='bg-primary h-full'>
       <FlatList 
-        data={[{ id: 1 }, { id: 2}, { id: 3}]}
+        data={[{id:1}, {id:2}, {id:3}]}
         keyExtractor={(item) => item.$id}
         renderItem={({item}) => (
           <Text className='text-3xl text-white'>{item.id}</Text>
@@ -24,13 +33,18 @@ const Home = () => {
                 <Image source={images.logoSmall} className='w-9 h-10' resizeMode='contain'/>
               </View>
             </View>
-            <SearchInput />
+            <SearchInput placeholder={'Serach for a video topic'}/>
             <View className='w-full flex-1 pt-5 pb-8'>
               <Text className='text-gray-100 text-lg mb-3'>Latest Videos</Text>
+              <Trending posts={[{id:1}, {id:2}, {id: 3}] ?? []}/>
             </View>
           </View>
         )}
-        />
+        ListEmptyComponent={()=>(
+          <EmptyState title='No Videos Found' subtitle='Be the first one to uploade video'/>
+        )}
+        refreshControl={<RefreshControl refreshing={refreshing} onRefresh={onRefresh}/>}
+      />
     </SafeAreaView>
   )
 }
